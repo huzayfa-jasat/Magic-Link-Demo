eae11a5b96d4c93b48183cea2ff3044d384f5005
Garfield <louie@levantalabs.com>
2025-05-23 17:00:50 -0400

    Fixed Side Bar

--- frontend/src/ui/Layouts/AppLayout/AppLayout.jsx
+++ frontend/src/ui/Layouts/AppLayout/AppLayout.jsx
@@ -1,11 +1,14 @@
 // Dependencies
 import { useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 // Style Imports
 import s from "./AppLayout.module.css";
 
+import { getBalance } from "../../../api/credits";
+
 // Functional Component
 export default function AppLayout({ title, children }) {
+  const { id } = useParams();
   // Change document title
   useEffect(() => {
     if (title) document.title = `${title} | OmniVerifier`;
@@ -55,7 +58,6 @@ export default function AppLayout({ title, children }) {
                 </svg>
                 Upload
               </NavLink>
-              {}
               <NavLink to="/packages" className={s.packagesButton}>
                 Buy Credits
               </NavLink>
@@ -84,7 +86,7 @@ export default function AppLayout({ title, children }) {
               >
                 <path d="M7.05,0H115.83a7.07,7.07,0,0,1,7,7.05V81.81a7,7,0,0,1-1.22,4,2.78,2.78,0,0,1-.66,1,2.62,2.62,0,0,1-.66.46,7,7,0,0,1-4.51,1.65H7.05a7.07,7.07,0,0,1-7-7V7.05A7.07,7.07,0,0,1,7.05,0Zm-.3,78.84L43.53,40.62,6.75,9.54v69.3ZM49.07,45.39,9.77,83.45h103L75.22,45.39l-11,9.21h0a2.7,2.7,0,0,1-3.45,0L49.07,45.39Zm31.6-4.84,35.46,38.6V9.2L80.67,40.55ZM10.21,5.41,62.39,47.7,112.27,5.41Z" />
               </svg>
-              500 Credits Left
+              {getBalance(id).data.credit_balance} Credits Left
             </NavLink>
             <NavLink to="/packages" className={s.packagesButtonBottom}>
               Buy Credits
